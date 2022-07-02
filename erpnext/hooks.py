193aad4f1cca4bb0310dafe40605213e4cdf352b
MohamedAbdulsalam <moha2016it@gmail.com>
2022-07-02 18:40:29 +0300

    URL hooks changed

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -8,7 +8,7 @@ app_icon = "fa fa-th"
 app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
-source_link = "https://github.com/frappe/erpnext"
+source_link = "https://github.com/MohamedAbdulsalam96/erpnext"
 app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
 
 
